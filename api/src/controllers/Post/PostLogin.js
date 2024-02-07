c002a42f6916fb6ac858a1d0ce49bd142fb40ce7
Erika Ladner <erikaladner5@gmail.com>
2024-02-06 21:42:59 -0300

    a ver

--- api/src/controllers/Post/PostLogin.js
+++ api/src/controllers/Post/PostLogin.js
@@ -4,26 +4,30 @@ const { Usuario, Cliente } = require('../../db');
 const bcrypt = require('bcrypt');
 
 const router = express.Router();
+
 // Método para autenticar un usuario
 router.post('/', async (req, res) => {
   const { usuario, contraseña } = req.body;
-
+  console.log(usuario, contraseña)
   try {
     // Buscar el usuario en la base de datos
     const user = await Usuario.findOne({
       where: { usuario },
     });
-
+    console.log(user)
     // Verificar si el usuario existe y si la contraseña es correcta
     if (!user || !bcrypt.compareSync(contraseña, user.contraseña)) {
       return res.status(401).json({ error: 'Usuario o contraseña incorrectos' });
     }
 
-    // Obtener el id_cliente asociado al usuario
-    const { id_cliente } = user;
+    // Actualizar la propiedad logueado a true
+    await Usuario.update({ logueado: true }, { where: { id_usuario: user.id_usuario } });
 
+    // Obtener el id_cliente asociado al usuario
+    const { clienteId } = user;
     // Buscar el cliente en la base de datos utilizando el id_cliente
-    const cliente = await Cliente.findByPk(id_cliente);
+    const cliente = await Cliente.findByPk(clienteId);
+    console.log(cliente)
 
     // Verificar si se encontró el cliente
     if (!cliente) {
