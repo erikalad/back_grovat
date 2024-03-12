9b9f362204a7b30db4e8258831ecec696abbc100
Erika Ladner <erikaladner5@gmail.com>
2024-03-12 17:10:32 -0300

    customizaciones

--- api/src/controllers/Post/PostCustomizaciones.js
+++ api/src/controllers/Post/PostCustomizaciones.js
@@ -3,7 +3,7 @@ const { Customizaciones, Cliente } = require('../../db');
 
 const router = express.Router();
 
-// Ruta para crear una customización
+// Ruta para crear o actualizar una customización
 router.post('/', async (req, res) => {
   try {
     // Obtener datos del cuerpo de la solicitud
@@ -11,27 +11,38 @@ router.post('/', async (req, res) => {
 
     // Verificar si existe el cliente con el clienteId proporcionado
     const clienteExistente = await Cliente.findByPk(clienteId);
-
     if (!clienteExistente) {
       return res.status(400).json({ mensaje: 'No se encontró el cliente con el clienteId proporcionado.' });
     }
 
-    // Crear la customización en la base de datos, vinculada al cliente
-    const nuevaCustomizacion = await Customizaciones.create({
-      nombreEmpresa,
-      colorPrincipal,
-      colorSecundario,
-      tipoLetra,
-      logoImg,
-      clienteId,
-    });
+    // Verificar si ya existe una customización para el cliente
+    const customizacionExistente = await Customizaciones.findOne({ where: { clienteId } });
 
-    // Enviar respuesta con la nueva customización creada
-    res.status(201).json({ mensaje: 'Customización creada con éxito', customizacion: nuevaCustomizacion });
+    if (customizacionExistente) {
+      // Actualizar la customización existente
+      await customizacionExistente.update({
+        nombreEmpresa,
+        colorPrincipal,
+        colorSecundario,
+        tipoLetra,
+        logoImg
+      });
+      res.status(200).json({ mensaje: 'Customización actualizada con éxito', customizacion: customizacionExistente });
+    } else {
+      // Crear una nueva customización
+      const nuevaCustomizacion = await Customizaciones.create({
+        nombreEmpresa,
+        colorPrincipal,
+        colorSecundario,
+        tipoLetra,
+        logoImg,
+        clienteId,
+      });
+      res.status(201).json({ mensaje: 'Customización creada con éxito', customizacion: nuevaCustomizacion });
+    }
   } catch (error) {
-    // Manejar errores y enviar respuesta al cliente
-    console.error('Error al crear customización:', error);
-    res.status(500).json({ mensaje: 'Error al crear customización', error: error.message });
+    console.error('Error al crear o actualizar customización:', error);
+    res.status(500).json({ mensaje: 'Error al crear o actualizar customización', error: error.message });
   }
 });
 
