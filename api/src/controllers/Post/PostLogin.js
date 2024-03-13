6e6bd797dfbf7ccae8da6ed79c149fe256f82af1
Erika Ladner <erikaladner5@gmail.com>
2024-03-13 18:33:07 -0300

    user activo

--- api/src/controllers/Post/PostLogin.js
+++ api/src/controllers/Post/PostLogin.js
@@ -1,19 +1,32 @@
-const express = require('express');
-const { Cliente, Usuario, Customizaciones, Funcionalidades } = require('../../db'); 
+const express = require("express");
+const {
+  Cliente,
+  Usuario,
+  Customizaciones,
+  Funcionalidades,
+} = require("../../db");
 
 const router = express.Router();
 
 // Método para autenticar un usuario
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   const { usuario, contraseña } = req.body;
   try {
     // Buscar el usuario en la base de datos
     const user = await Usuario.findOne({
       where: { usuario },
     });
+
+    // Verificar si el usuario está activo
+    if (!user.activo) {
+      return res.status(401).json({ error: "Tu usuario no está activo" });
+    }
+
     // Verificar si el usuario existe y si la contraseña es correcta
     if (!user || user.contraseña !== contraseña) {
-      return res.status(401).json({ error: 'Usuario o contraseña incorrectos' });
+      return res
+        .status(401)
+        .json({ error: "Usuario o contraseña incorrectos" });
     }
 
     // // Verificar si el usuario ya está logueado en otro dispositivo
@@ -22,7 +35,10 @@ router.post('/', async (req, res) => {
     // }
 
     // Actualizar la propiedad logueado a true
-    await Usuario.update({ logueado: true }, { where: { id_usuario: user.id_usuario } });
+    await Usuario.update(
+      { logueado: true },
+      { where: { id_usuario: user.id_usuario } }
+    );
 
     // Obtener el id_cliente asociado al usuario
     const { clienteId } = user;
@@ -36,14 +52,14 @@ router.post('/', async (req, res) => {
     });
     // Verificar si se encontró el cliente
     if (!cliente) {
-      return res.status(404).json({ error: 'Cliente no encontrado' });
+      return res.status(404).json({ error: "Cliente no encontrado" });
     }
 
     // Si se encuentra el cliente, devolverlo como respuesta
     return res.json(cliente);
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ error: 'Error del servidor' });
+    return res.status(500).json({ error: "Error del servidor" });
   }
 });
 
